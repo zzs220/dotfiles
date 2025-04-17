b31946dc4493ec14f0716d5ffb5185666665a582
zrd <zrd@localhost.com>
2025-04-17 17:23:04 +0700

    nvim: toggle virtual lines with leader+k

--- nvim/lua/general/config.lua
+++ nvim/lua/general/config.lua
@@ -14,7 +14,14 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "<leader>ct", ":lua Transparent()<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>rn", ":lua  vim.o.relativenumber = not vim.o.relativenumber<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>mm", ":lua  ToggleMouse()<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>k", ":lua  vim.diagnostic.open_float()<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>k", ":lua  ToggleVL()<CR>", { noremap = true })
+
+vim.diagnostic.config({virtual_text=true})
+VirtualLineEnabled = false
+function ToggleVL()
+    VirtualLineEnabled = not VirtualLineEnabled
+    vim.diagnostic.config({virtual_lines = VirtualLineEnabled, virtual_text=not VirtualLineEnabled})
+end
 
 function ToggleMouse()
     if o.mouse._value == 'a' then
