c599b3f2c4b83177cedbd094249552965e7127fd
zrd <zrd@localhost.com>
2025-04-17 17:26:06 +0700

    nvim: blink.cmp migration -> remove lsp-zero

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -6,17 +6,10 @@ return{
     },
     config = function ()
         require('mason').setup()
-        local lsp_zero = require('lsp-zero')
         require('mason-lspconfig').setup({
             handlers = {
                 function(server_name)
                     require('lspconfig')[server_name].setup({})
-                end,
-                lsp_zero.default_setup,
-                lua_ls = function()
-                    -- (Optional) configure lua language server
-                    local lua_opts = lsp_zero.nvim_lua_ls()
-                    require('lspconfig').lua_ls.setup(lua_opts)
                 end,
             }
         })
