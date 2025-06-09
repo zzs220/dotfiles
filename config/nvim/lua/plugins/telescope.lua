7ba86628b6dabc435e8a944e18fae99b38071b8f
zrd <zrd@localhost.com>
2025-06-09 20:57:31 +0700

    added telescope some keybinds

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,8 @@ return{
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fq', builtin.resume, {})
+        vim.keymap.set('n', '<leader>fs', builtin.treesitter, {})
         vim.keymap.set('n', '<leader>fi', builtin.lsp_implementations, {})
         vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
         vim.keymap.set('n', '<leader>ft', ':Telescope<CR>', {})
