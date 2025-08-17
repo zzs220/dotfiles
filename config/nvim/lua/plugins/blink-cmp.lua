3595998118bb6f19dc8725d0a73b92c1fb4e5ec6
zrd <zrd@localhost.com>
2025-08-17 11:02:24 +0700

    nvim: first tab select the first completion item instead of skipping it

--- config/nvim/lua/plugins/blink-cmp.lua
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -1,49 +1,48 @@
 return
 {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = '1.*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
 
-  event = 'VimEnter',
-  opts = {
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = {
-        preset = 'none',
-        ['<C-j>'] = {'select_next', 'fallback'},
-        ['<C-k>'] = {'select_prev', 'fallback'},
-        ['<CR>'] = {'accept', 'fallback'},
-        ['<C-d>'] = {'scroll_documentation_down'},
-        ['<C-u>'] = {'scroll_documentation_up'},
-        ["<Tab>"] = {
-            function(cmp)
-                return cmp.select_next()
-            end,
-            "snippet_forward",
-            "fallback",
+    event = 'VimEnter',
+    opts = {
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = {
+            preset = 'none',
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<C-j>'] = { 'select_next', 'fallback' },
+            ['<C-k>'] = { 'select_prev', 'fallback' },
+            ['<CR>'] = { 'accept', 'fallback' },
+            ['<C-d>'] = { 'scroll_documentation_down' },
+            ['<C-u>'] = { 'scroll_documentation_up' },
+            ["<Tab>"] = {
+                "select_next",
+                "snippet_forward",
+                "fallback",
+            },
+            ["<S-Tab>"] = {
+                "select_prev",
+                "snippet_backward",
+                "fallback",
+            },
         },
-        ["<S-Tab>"] = {
-            function(cmp)
-                return cmp.select_prev()
-            end,
-            "snippet_backward",
-            "fallback",
+        appearance = {
+            nerd_font_variant = 'mono'
         },
+        completion = {
+            list = { selection  = {preselect = false}},
+            documentation = { auto_show = true },
+            accept = { auto_brackets = { enabled = false } },
+        },
+        signature = { enabled = true },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" }
     },
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-    completion = {
-        documentation = { auto_show = true },
-        accept = {auto_brackets = {enabled = false}},
-    },
-    signature = {enabled = true},
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" }
 }
