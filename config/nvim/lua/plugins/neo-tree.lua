cbc7109e14d4e72a31fbfedcf22aefe769eee674
zrd <zrd@localhost.com>
2025-08-26 15:43:24 +0700

    nvim: neotree, follow curently open file

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -6,7 +6,22 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function ()
-        vim.api.nvim_set_keymap("n", "<leader>e", ":Neotree toggle float<CR>", { noremap = true })
-        vim.api.nvim_set_keymap("n", "<leader>E", ":Neotree left<CR>", { noremap = true })
+        vim.api.nvim_set_keymap("n", "<leader>e", ":Neotree toggle reveal float<CR>", { noremap = true })
+        vim.api.nvim_set_keymap("n", "<leader>E", ":Neotree reveal left<CR>", { noremap = true })
+        require("neo-tree").setup({
+            filesystem = {
+                -- filtered_items = {
+                --     visible = true,
+                --     show_hidden_count = true,
+                --     hide_dotfiles = false,
+                --     hide_gitignored = false,
+                -- },
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = false,
+                },
+            },
+            buffers = { follow_current_file = { enable = true } },
+        })
     end
 }
