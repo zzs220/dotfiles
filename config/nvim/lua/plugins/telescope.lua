3ca51e5d31e754345c449b47d416f33c6df8ee4b
zrd <zrd@localhost.com>
2026-04-04 02:09:11 +0000

    nvim: package versioning + lock update

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,9 @@
 return{
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    version = '*',
+    dependencies = { 'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+},
     config = function ()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,5 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
     build = ":TSUpdate",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
