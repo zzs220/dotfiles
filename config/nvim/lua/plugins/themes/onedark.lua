25f05cfba3d00c4c5926ae0199af92fa701f9b45
zrd <zrd@localhost.com>
2026-04-13 17:12:29 +0000

    nvim: onedark, make other menu transparent too

--- config/nvim/lua/plugins/themes/onedark.lua
+++ config/nvim/lua/plugins/themes/onedark.lua
@@ -4,7 +4,14 @@ return {
   config = function()
     require('onedark').setup({
       transparent = true,
-      style = 'darker',
+      style = 'cool',
+      highlights = {
+        NormalFloat = { bg = 'None' },
+        FloatBorder = { bg = 'None' },
+        Pmenu = { bg = 'None' },
+        PmenuSel = { bg = '#3e4451' },
+        PmenuSbar = { bg = '#2c313a' },
+      },
     })
     require('onedark').load()
   end
