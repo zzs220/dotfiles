6dc966c7da837e26f5ae990f26f8d0161bd6e59f
zrd <zrd@localhost.com>
2026-08-14 10:12:20 +0000

    hypr: added next/prev workspace navigaton

--- config/hypr/hyprconf/keybinds.lua
+++ config/hypr/hyprconf/keybinds.lua
@@ -54,6 +54,9 @@ for i = 1, 10 do
     hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
 end
 
+hl.bind(mainMod .. " + N", hl.dsp.focus({workspace = "e+1"}))
+hl.bind(mainMod .. " + P", hl.dsp.focus({workspace = "e-1"}))
+
 -- Example special workspace (scratchpad)
 hl.bind(mainMod .. " + S",         hl.dsp.workspace.toggle_special("magic"))
 hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
